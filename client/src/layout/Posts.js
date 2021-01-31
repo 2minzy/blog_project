858019b4e6c5805c9ba10b9aac14837ebc80f46d
Minji <minjilee0423@gmail.com>
2021-02-01 00:51:47 +0900

    Added lodash-es and excerpt to blog post preview

--- client/src/layout/Posts.js
+++ client/src/layout/Posts.js
@@ -4,6 +4,7 @@ import Card from "../components/Card"
 import CardContent from "../components/CardContent"
 import Content from "../components/Content"
 import lemon from "../../static/lemon.jpg"
+import { truncate } from "lodash-es"
 
 const Posts = ({ posts }) => {
   return (
@@ -14,7 +15,7 @@ const Posts = ({ posts }) => {
             <img src={lemon} alt="" />
             <CardContent>
               <h3>{post.title}</h3>
-              <p>{post.body}</p>
+              <p>{truncate(post.body, { length: 120, separator: " " })}</p>
             </CardContent>
           </Card>
         ))}
